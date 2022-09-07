ce646e51f6050dd6982a801d39714e39b31bac41
PiyushMittal <20piyushmittal@gmail.com>
2022-09-07 08:26:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,17 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    })
+}
+            
+
 app.use(notFound);
 app.use(errorHandler);
 
